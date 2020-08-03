7d8aeddbda56eb5e963b8c883a58b1b0331af4da
litTaoTao <1099683985@qq.com>
2020-08-03 17:08:58 +0800

    1.分环境打包 2.修改route和store结构 3.hash模式

---
+++ src/router/modules/about.js
@@ -0,0 +1,9 @@
+const claimSearch = [{
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( /* webpackChunkName: "about" */ '@/views/About.vue')
+}];
+export default claimSearch;
