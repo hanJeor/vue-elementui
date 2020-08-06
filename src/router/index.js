eea775b1c456242f4e0482058893c04c25e08704
litTaoTao <1099683985@qq.com>
2020-08-06 16:41:09 +0800

    增加NProgress

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from "@/views/Login.vue"
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+// 简单配置
+NProgress.inc(0.2)
+NProgress.configure({ easing: 'ease', speed: 500, showSpinner: false })
 
 Vue.use(Router)
 
@@ -27,6 +32,7 @@ const router = new Router({
 })
 
 router.beforeEach((to, from, next) => {
+    NProgress.start();
     if(to.path === '/login'){
         sessionStorage.removeItem('user');
     }
@@ -38,6 +44,10 @@ router.beforeEach((to, from, next) => {
     }else{
         next();
     }
+})
+
+router.afterEach(() => {
+  NProgress.done()
 })
 
 const originalPush = Router.prototype.push
