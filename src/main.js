5e34bad35877cb49651f8496e4ec347fe13d733a
litTaoTao <1099683985@qq.com>
2020-08-06 15:06:08 +0800

    修改了点东西

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
-import router from './router';
+import router from './router/index.js';
 import ElementUI from "element-ui";
 import 'element-ui/lib/theme-chalk/index.css';
 import "./assets/css/global.css";
